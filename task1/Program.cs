9d393b4be58b25f1d796d3d7751ded288532dc05
Natalya Skorikova <skorikova@list.ru>
2023-11-18 23:01:04 +0500

    Initial commit

---
+++ task1/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 5 -> "2, 4"
+// M = 4; N = 8 -> "4, 6, 8"
+
+int ReadInt(string mes)
+{
+    Console.WriteLine(mes);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+void PrintEvenNum(int M, int N)
+{
+    if (M <= N)
+    {
+        if(M%2 == 0)
+        {
+        System.Console.Write($"{M}\t");
+        PrintEvenNum(M + 2, N);
+        }
+        else
+        {
+        PrintEvenNum(M + 1, N);
+        }
+    }
+}
+
+int M = ReadInt("Введите первое число");
+int N = ReadInt("Введите второе число");
+PrintEvenNum(M, N);
+
+
---
+++ task2/Program.cs
@@ -0,0 +1,30 @@
+﻿//Задача 2: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N с помощью рекурсии.
+
+
+int ReadInt(string mes)
+{
+    Console.WriteLine(mes);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+int SumNumbers (int M, int N)
+{
+    if(N < M)
+    {
+        return 0;
+    }
+   return SumNumbers(M, N-1) + N;
+   System.Console.Write($"{M}\t");
+   
+    
+}
+int M = ReadInt("Введите первое число");
+int N = ReadInt("Введите второе число");
+Console.WriteLine(SumNumbers(M,N));
+
+
+
+
+
+
